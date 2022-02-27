4464ac9559e07089eecb0930968bf86f00406ff3
Manuel <trapecioinv@gmail.com>
2022-02-26 23:31:32 -0300

    PROGRESO: Agregadas validaciones con pattern en HTML. Arregladas validaciones con JavaScript. TODO: Mensaje personalizado en checkbox.

--- js/forms.js
+++ js/forms.js
@@ -1,33 +1,38 @@
-
 // ===== Otorgamos el valor INDETERMINADO a un checkbox =====
 document.getElementById("threeCB").indeterminate = true;
 // ==========================================================
 
 // ===== Creamos un NODELIST con los checkbox que queremos validar.=====
-const requiredCheckboxes = document.querySelectorAll('.requiredCheckbox');
+const requiredCheckboxes = document.querySelectorAll(".requiredCheckbox");
 
 // ===== Transformamos la NODELIST en un ARRAY para poder aplicar los métodos SOME y FOREACH.=====
 const arrCheckboxes = [].slice.call(requiredCheckboxes);
 
 // ===== Asignamos el boton a una variable.=====
-const submitButton = document.querySelector('button[type="submit"]')
+const submitButton = document.querySelector('button[type="submit"]');
 
-
-// ===== Creamos una función que verifique si UNO de los checkbox está seleccionado.=====
+/* ===== 
+Creamos una función que verifique si UNO de los checkbox está seleccionado. 
+Si el resultado es TRUE, remueve el atributo required de TODOS los checkbox, caso contrario devuelve false.===== */
 function oneIsChecked() {
-    return arrCheckboxes.some(function (cb) {
-        return cb.checked;
-    });
+    if (arrCheckboxes.some(checkbox => checkbox.checked)) {
+        arrCheckboxes.forEach(checkbox => checkbox.removeAttribute('required'));
+        return true;
+    }
+    return false;
 }
 
 // ===== Creamos una función que REMUEVE el atributo required a TODOS los elementos si uno de los checkbox está seleccionado.=====
 function validateCheckboxes() {
-    if (oneIsChecked()) {
-        arrCheckboxes.forEach(element => {
-            element.removeAttribute('required');
-        });
+    if (!oneIsChecked()) {
+        arrCheckboxes.forEach(el => el.setAttribute('required', ''))
+        return false;
     }
+    return true;
 }
+
+
+
 
 // ===== Llamamos la función con el botón cuando le hacemos click.=====
 submitButton.addEventListener("click", validateCheckboxes);
