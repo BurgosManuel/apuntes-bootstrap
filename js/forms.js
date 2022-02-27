7961ebe8674546cdd6d8fad8b695c409a08329b6
Manuel <trapecioinv@gmail.com>
2022-02-26 23:48:20 -0300

    PROGRESO: Mensajes personalizados en checkbox. TODO: FFinalizar sección final.

--- js/forms.js
+++ js/forms.js
@@ -13,7 +13,7 @@ const submitButton = document.querySelector('button[type="submit"]');
 
 /* ===== 
 Creamos una función que verifique si UNO de los checkbox está seleccionado. 
-Si el resultado es TRUE, remueve el atributo required de TODOS los checkbox, caso contrario devuelve false.===== */
+Si el resultado es TRUE, remueve el atributo required de TODOS los checkbox y retorna true, caso contrario retorna false.===== */
 function oneIsChecked() {
     if (arrCheckboxes.some(checkbox => checkbox.checked)) {
         arrCheckboxes.forEach(checkbox => checkbox.removeAttribute('required'));
@@ -22,16 +22,24 @@ function oneIsChecked() {
     return false;
 }
 
-// ===== Creamos una función que REMUEVE el atributo required a TODOS los elementos si uno de los checkbox está seleccionado.=====
+/* ===== 
+    Creamos una función para validar los checkboxes y agregar mensajes personalizados.
+    Si no hay ninguno checkeado, devuelve el atributo required a todos los elementos y le otorga un mensaje persoanlizado.
+    Si hay al menos uno checkeado (los required ya no están gracias a la función oneIsChecked), remueve el mensaje personalizado y valida el formulario.
+===== */
 function validateCheckboxes() {
     if (!oneIsChecked()) {
-        arrCheckboxes.forEach(el => el.setAttribute('required', ''))
+        arrCheckboxes.forEach(el => {
+            el.setAttribute('required', '');
+            el.setCustomValidity('Elige al menos una opción.');
+        })
         return false;
     }
+    arrCheckboxes.forEach(el => {
+        el.setCustomValidity('');
+    })
     return true;
 }
-
-
 
 
 // ===== Llamamos la función con el botón cuando le hacemos click.=====
